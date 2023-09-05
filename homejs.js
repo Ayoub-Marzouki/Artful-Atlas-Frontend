b6207d57ab2662521fd71669b4dbd11dc0de6ade
Ayoub Marzouki <ayoubma321@gmail.com>
2023-09-05 03:01:05 +0100

    More animations, more screen size adjustments

--- homejs.js
+++ homejs.js
@@ -51,13 +51,13 @@ function createSlider(sliderId, intervalTime) {
     leftArrow.addEventListener('click', () => {
         pauseAutoSlide();
         previousSlide();
-        resumeAutoSlide();
+        slider.focus();
     });
 
     rightArrow.addEventListener('click', () => {
         pauseAutoSlide();
         nextSlide();
-        resumeAutoSlide();
+        slider.focus();
     });
 
     // Function to start the slider when it becomes visible
@@ -85,3 +85,46 @@ function createSlider(sliderId, intervalTime) {
 // Call the function to create the sliders
 createSlider('slider', 2000); // First slider
 createSlider('slider2', 2000); // Second slider
+
+
+// Create function to add the animation for the logo
+function startLogoAnimationWhenVisible(entries,observer) {
+    entries.forEach(entry=> {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('logo-animation');
+
+            observer.unobserve(entry.target);
+        }
+    })
+}
+
+// Create function to add the animation for the Heart span element
+function startHeartAnimationWhenVisible(entries,observer) {
+    if (entries[0].isIntersecting) {
+        entries[0].target.classList.add('heartgradient-animation');
+        observer.unobserve(entries[0].target);
+    }
+}
+// Define the visibility options that both functions will use
+const observerOptions1 = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+};
+const observerOptions2 = {
+    root:null,
+    rootMargin:"-220px",
+    threshold:1,
+}
+
+// Create a single Intersection Observer object for both functions
+const logoObserver = new IntersectionObserver(startLogoAnimationWhenVisible, observerOptions1);
+const heartObserver = new IntersectionObserver(startHeartAnimationWhenVisible,observerOptions2);
+
+// Look for logo and call the function upon it
+const logo=document.getElementsByClassName("logo");
+logoObserver.observe(logo[0]);
+
+// Look for the heart span and call the function upon it
+const heartGradient=document.getElementById("heartgradient");
+heartObserver.observe(heartGradient);
