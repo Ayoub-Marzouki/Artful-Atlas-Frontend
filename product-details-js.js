c0013f3c0b6b66aa4007bd8bdbacde7a06dddd7c
Ayoub Marzouki <ayoubma321@gmail.com>
2024-04-08 01:28:58 +0000

    Modified the product-details page. - Forced the secondary images to NOT surpass their container's width (This way if the user enters a lot of secondary images, they'll remain in their nice place instead of ruining the page's layout).
    - After clicking on one of the secondary images, it takes the place of the main image, not as a slider but as a fade off and in effect.
    - Added the main image as a secondary image, so that in case the user chooses check the other images, he can still go back to check the main image.

--- product-details-js.js
+++ product-details-js.js
@@ -27,3 +27,36 @@ artworkImage.addEventListener('click', function(event) {
     const imageUrl = this.src;
     createLightbox(imageUrl);
 });
+
+const secondaryImages = document.getElementsByClassName('secondary-images');
+for (const image of secondaryImages) {
+    image.addEventListener('click', function(event) {
+        // Prevent the click event from bubbling up to the profile artwork
+        event.stopPropagation();
+        const imageUrl = this.src;
+        updateMainImage(imageUrl);
+
+    });
+}
+
+// Function to update the main image with the clicked secondary image
+function updateMainImage(imageUrl) {
+    if (artworkImage.src === imageUrl) {
+        return; // Exit the function early if it's the same image
+    }
+    // Smoothly fade out the image
+    artworkImage.style.transition = 'opacity 1s';
+    artworkImage.style.opacity = '0';
+    
+    // Wait for the fade-out effect to complete
+    setTimeout(function() {
+        // Change the image source
+        artworkImage.src = imageUrl;
+        
+        // Smoothly fade in the new image
+        artworkImage.style.opacity = '1';
+    }, 1000); // Adjust the delay as needed
+}
+
+
+
